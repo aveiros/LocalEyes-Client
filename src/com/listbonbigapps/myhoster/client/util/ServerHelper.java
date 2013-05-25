8321540397cc4f2230078c12a9142767ab0f12e6
aveiros <aqviveiros@gmail.com>
2013-05-25 18:14:38 +0100

    updated server rest url

--- src/com/listbonbigapps/myhoster/client/util/ServerHelper.java
+++ src/com/listbonbigapps/myhoster/client/util/ServerHelper.java
@@ -1,7 +1,7 @@
 package com.listbonbigapps.myhoster.client.util;
 
 public class ServerHelper {
-	private static String ServerIp = "192.168.1.67";
+	private static String ServerIp = "myhoster.dyndns.info";
 	private static String ServerPort = "8080";
 	private static String ServerRestUrl = "http://" + ServerIp + ":"
 			+ ServerPort + "/myHoster/rest";
