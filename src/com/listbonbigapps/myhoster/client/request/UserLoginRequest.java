4184e9fcd5dd44b31f7ed88fe909692958fc85df
aveiros <aqviveiros@gmail.com>
2013-06-05 12:40:22 +0100

    extended SpringAndroidSpiceRequest, now uses the same HttpClient for each http rest; refactored UserLogin/Logout Requests

--- src/com/listbonbigapps/myhoster/client/request/UserLoginRequest.java
+++ src/com/listbonbigapps/myhoster/client/request/UserLoginRequest.java
@@ -1,38 +1,31 @@
 package com.listbonbigapps.myhoster.client.request;
 
-import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
 
 import android.net.Uri;
 
 import com.listbonbigapps.myhoster.client.resources.UserResource;
-import com.listbonbigapps.myhoster.client.util.HttpClientSingleton;
+import com.listbonbigapps.myhoster.client.spring.SpringAndroidSpiceRequestExtended;
 import com.listbonbigapps.myhoster.client.util.ServerHelper;
-import com.octo.android.robospice.request.springandroid.SpringAndroidSpiceRequest;
 
-public class UserLoginRequest extends SpringAndroidSpiceRequest<UserResource> {
-	private String username;
-	private String password;
+public class UserLoginRequest extends SpringAndroidSpiceRequestExtended<UserResource> {
+    private String username;
+    private String password;
 
-	public UserLoginRequest(String username, String password) {
-		super(UserResource.class);
-		this.username = username;
-		this.password = password;
-	}
+    public UserLoginRequest(String username, String password) {
+	super(UserResource.class);
+	this.username = username;
+	this.password = password;
+    }
 
-	@Override
-	public UserResource loadDataFromNetwork() throws Exception {
-		Uri.Builder uriBuilder = Uri.parse(
-				ServerHelper.buildRestUrl("/user/login")).buildUpon();
-		uriBuilder.appendQueryParameter("username", this.username);
-		uriBuilder.appendQueryParameter("password", this.password);
-		String url = uriBuilder.build().toString();
+    @Override
+    public UserResource loadDataFromNetwork() throws Exception {
+	Uri.Builder uriBuilder = Uri.parse(ServerHelper.buildRestUrl("/user/login")).buildUpon();
+	uriBuilder.appendQueryParameter("username", this.username);
+	uriBuilder.appendQueryParameter("password", this.password);
+	String url = uriBuilder.build().toString();
 
-		RestTemplate restTemplate = this.getRestTemplate();
-		HttpComponentsClientHttpRequestFactory requestFactory = new HttpComponentsClientHttpRequestFactory(
-				HttpClientSingleton.getInstance());
-		restTemplate.setRequestFactory(requestFactory);
-
-		return restTemplate.getForObject(url, UserResource.class);
-	}
+	RestTemplate restTemplate = this.getRestTemplate();
+	return restTemplate.getForObject(url, UserResource.class);
+    }
 }
--- src/com/listbonbigapps/myhoster/client/request/UserLogoutRequest.java
+++ src/com/listbonbigapps/myhoster/client/request/UserLogoutRequest.java
@@ -1,31 +1,24 @@
 package com.listbonbigapps.myhoster.client.request;
 
-import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
 
 import android.net.Uri;
 
 import com.listbonbigapps.myhoster.client.resources.MessageResource;
-import com.listbonbigapps.myhoster.client.util.HttpClientSingleton;
+import com.listbonbigapps.myhoster.client.spring.SpringAndroidSpiceRequestExtended;
 import com.listbonbigapps.myhoster.client.util.ServerHelper;
-import com.octo.android.robospice.request.springandroid.SpringAndroidSpiceRequest;
 
-public class UserLogoutRequest extends SpringAndroidSpiceRequest<MessageResource> {
-	public UserLogoutRequest() {
-		super(MessageResource.class);
-	}
+public class UserLogoutRequest extends SpringAndroidSpiceRequestExtended<MessageResource> {
+    public UserLogoutRequest() {
+	super(MessageResource.class);
+    }
 
-	@Override
-	public MessageResource loadDataFromNetwork() throws Exception {
-		Uri.Builder uriBuilder = Uri.parse(ServerHelper.buildRestUrl("/user/logout"))
-				.buildUpon();
-		String url = uriBuilder.build().toString();
+    @Override
+    public MessageResource loadDataFromNetwork() throws Exception {
+	Uri.Builder uriBuilder = Uri.parse(ServerHelper.buildRestUrl("/user/logout")).buildUpon();
+	String url = uriBuilder.build().toString();
 
-		RestTemplate restTemplate = this.getRestTemplate();
-		HttpComponentsClientHttpRequestFactory requestFactory = new HttpComponentsClientHttpRequestFactory(
-				HttpClientSingleton.getInstance());
-		restTemplate.setRequestFactory(requestFactory);
-
-		return restTemplate.getForObject(url, MessageResource.class);
-	}
+	RestTemplate restTemplate = this.getRestTemplate();
+	return restTemplate.getForObject(url, MessageResource.class);
+    }
 }
